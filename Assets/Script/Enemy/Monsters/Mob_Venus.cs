1b5959744436ecfb446123dce3a9a487d56afc89
TwoNight0 <wlsdn0658@gmail.com>
2024-01-06 00:47:14 +0900

    몬스터 스크립트 만들어서 넣어주는 것완료 및 라운드 시작과 끝 구현 생각중

---
+++ Assets/Script/Enemy/Monsters/Mob_Venus.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mob_Venus : Enemy
+{
+    Mob_Venus()
+    {
+        maxHp = 100f;
+        startSpeed = 10.0f;
+
+        //보상
+        reward = 10;
+
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nowHp = maxHp;
+        speed = startSpeed;
+
+        //HpBar 컨트롤
+        this.hpBarObj = GameObject.Find("HpCanvas");
+        hpBar = hpBarObj.GetComponent<RectTransform>();
+
+        //hp Bar 텍스트 가져오기 -> 피해 입으면 슬라이더랑 다깍기
+
+
+        waypointindex++;
+
+        //타겟지정(걸어갈 목표)
+        target = GameManager.instance.waypoints[waypointindex];
+
+        transform.LookAt(target);
+
+
+        Debug.Log(hpBar);
+
+        //this.rotateHpbar(hpBar);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveToTarget(this.transform, speed);
+    }
+}
--- Assets/Script/Manager/MngEnemy.cs
+++ Assets/Script/Manager/MngEnemy.cs
@@ -8,13 +8,11 @@ public class MngEnemy : MonoBehaviour
 {
     [Header("몬스터 프리팹")]
     //----Transform
-    public Transform[] enemyPrefabs;
+    public List<GameObject> enemyPrefabs;
     public Transform spawnPoint;
 
     //----bool
     //private bool isWave = false;
-
-    
 
     //----float
     [Header("WaveTimer")]
@@ -26,8 +24,42 @@ public class MngEnemy : MonoBehaviour
 
     //----int
     //10웨이브까지, 4웨이브면 보스 1,2,3,4
-    private int waveIndex = 0;
 
+    #region 몬스터 넘버링
+    private int mob_Bat = -1;
+    private int mob_Bee = -1;
+    private int mob_Beholder = -1;
+    private int mob_Bishopknight = -1;
+    private int mob_BlackKnight = -1;
+    private int mob_Cactus = -1;
+    private int mob_Chest = -1;
+    private int mob_Crab = -1;
+    private int mob_Cyclops = -1;
+    private int mob_Demonking = -1;
+    private int mob_Dragon = -1;
+    private int mob_EvilMage = -1;
+    private int mob_Fishman = -1;
+    private int mob_FylingDemon = -1;
+    private int mob_Golem = -1;
+    private int mob_LizardWarrior = -1;
+    private int mob_MushroomAngry = -1;
+    private int mob_MushroomSmile = -1;
+    private int mob_NageWizard = -1;
+    private int mob_Orc = -1;
+    private int mob_RatAssassin = -1;
+    private int mob_Salamander = -1;
+    private int mob_Skeleton = -1;
+    private int mob_Slime = -1;
+    private int mob_Specter = -1;
+    private int mob_Spider = -1;
+    private int mob_StringRay = -1;
+    private int mob_TurtleShell = -1;
+    private int mob_VenusPlant = -1;
+    private int mob_Wolf = -1;
+    private int mob_Worm = -1;
+    #endregion
+
+    private int waveIndex = 0;
 
     public enum WaveThema
     {
@@ -35,6 +67,11 @@ public class MngEnemy : MonoBehaviour
         Default,//일반
         AIR,//공중 혼합
         
+    }
+
+    private void Awake()
+    {
+        InitMonsterNumbering();
     }
 
     private void Start()
@@ -51,11 +88,9 @@ public class MngEnemy : MonoBehaviour
     private void Update()
     {
         //SpawnWave();
-
         //WaveTimer(countdown);
     }
 
-   
 
     /// <summary>
     /// 웨이브 소환
@@ -91,9 +126,13 @@ public class MngEnemy : MonoBehaviour
             {
                 case 1:
                     //몬스터 소환
-                    GameObject insMob = Instantiate(enemyPrefabs[0], spawnPoint.position, spawnPoint.rotation).gameObject;
-                    insMob.transform.parent = GameManager.instance.activePool;
-                    yield return new WaitForSeconds(0.2f);
+                    GameObject insMob_bat1 = Instantiate(enemyPrefabs[mob_Bat], spawnPoint.position, spawnPoint.rotation).gameObject;
+                    insMob_bat1.transform.parent = GameManager.instance.activePool;
+
+                    yield return new WaitForSeconds(0.5f);
+
+                    GameObject insMob_bat = Instantiate(enemyPrefabs[mob_Bat], spawnPoint.position, spawnPoint.rotation).gameObject;
+                    insMob_bat1.transform.parent = GameManager.instance.activePool;
 
                     break;
                 case 2:
@@ -108,6 +147,7 @@ public class MngEnemy : MonoBehaviour
                     break;
                 case 4:
                     break;
+
 #endregion
             }
         }
@@ -144,5 +184,52 @@ public class MngEnemy : MonoBehaviour
        
         yield return new WaitForSeconds(1.0f);
     }
+
+    #region 몬스터에 코드 부여하기
+    /// <summary>
+    /// 몬스터 넘버를 부여해 좀더 코드를 볼때 수월하게함
+    /// </summary>
+    private void InitMonsterNumbering()
+    {
+        mob_Bat = asignNumberToMonster("mob_Bat");
+        mob_Bee = asignNumberToMonster("mob_Bee");
+        mob_Beholder = asignNumberToMonster("mob_Beholder");
+        mob_Bishopknight = asignNumberToMonster("mob_Bishopknight");
+        mob_BlackKnight = asignNumberToMonster("mob_BlackKnight");
+        mob_Cactus = asignNumberToMonster("mob_Cactus");
+        mob_Chest = asignNumberToMonster("mob_Chest");
+        mob_Crab = asignNumberToMonster("mob_Crab");
+        mob_Cyclops = asignNumberToMonster("mob_Cyclops"); ;
+        mob_Demonking = asignNumberToMonster("mob_Demonking");
+        mob_Dragon = asignNumberToMonster("mob_Dragon");
+        mob_EvilMage = asignNumberToMonster("mob_EvilMage");
+        mob_Fishman = asignNumberToMonster("mob_Fishman");
+        mob_FylingDemon = asignNumberToMonster("mob_FylingDemon");
+        mob_Golem = asignNumberToMonster("mob_Golem");
+        mob_LizardWarrior = asignNumberToMonster("mob_LizardWarrior");
+        mob_MushroomAngry = asignNumberToMonster("mob_MushroomAngry");
+        mob_MushroomSmile = asignNumberToMonster("mob_MushroomSmile");
+        mob_NageWizard = asignNumberToMonster("mob_NageWizard");
+        mob_Orc = asignNumberToMonster("mob_Orc");
+        mob_RatAssassin = asignNumberToMonster("mob_RatAssassin");
+        mob_Salamander = asignNumberToMonster("mob_Salamander");
+        mob_Skeleton = asignNumberToMonster("mob_Skeleton");
+        mob_Slime = asignNumberToMonster("mob_Slime");
+        mob_Specter = asignNumberToMonster("mob_Specter");
+        mob_Spider = asignNumberToMonster("mob_Spider");
+        mob_StringRay = asignNumberToMonster("mob_StringRay");
+        mob_TurtleShell = asignNumberToMonster("mob_TurtleShell");
+        mob_VenusPlant = asignNumberToMonster("mob_VenusPlant");
+        mob_Wolf = asignNumberToMonster("mob_Wolf");
+        mob_Worm = asignNumberToMonster("mob_Worm");
+    }
+
+    private int asignNumberToMonster(string monsterStringName)
+    {
+        return enemyPrefabs.FindIndex(x => x.name == monsterStringName);
+    }
+
+    #endregion
+
 
 }
